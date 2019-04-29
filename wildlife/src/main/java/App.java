5d20224600a1b158375fb17b73b5f5152b57f623
Marvin Zachary <acerzach@gmail.com>
2019-04-29 21:14:12 +0300

    imports to app.java

--- wildlife/src/main/java/App.java
+++ wildlife/src/main/java/App.java
@@ -1,22 +1,22 @@
-// import java.util.Map;
-// import java.util.HashMap;
-// import java.util.List;
-// import java.util.ArrayList;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import static spark.Spark.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
-// public class App {
-//     public static void main(String[] args) {
-//       ProcessBuilder process = new ProcessBuilder();
-//          Integer port;
-//          if (process.environment().get("PORT") != null) {
-//              port = Integer.parseInt(process.environment().get("PORT"));
-//          } else {
-//              port = 4567;
-//          }
+public class App {
+    public static void main(String[] args) {
+      ProcessBuilder process = new ProcessBuilder();
+         Integer port;
+         if (process.environment().get("PORT") != null) {
+             port = Integer.parseInt(process.environment().get("PORT"));
+         } else {
+             port = 4567;
+         }
     
-//         setPort(port);
+        setPort(port);
 
 //         staticFileLocation("/public");
 //         String layout = "templates/layout.vtl";
